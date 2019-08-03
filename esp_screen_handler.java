57448a25e327f6d21fcf252426b877391cb0dcf1
rwb-git <38397737+rwb-git@users.noreply.github.com>
2019-08-03 13:05:24 -0400

    Update esp_screen_handler.java

--- esp_screen_handler.java
+++ esp_screen_handler.java
@@ -5,19 +5,9 @@ import android.os.Message;
 
 public class esp_screen_handler   extends Handler {
 
-    public esp_screen_handler() {
-	}
-
 	@Override
 	public void handleMessage(Message msg) {
 
-        switch (msg.what) {
-
-            case 4:
-
-                MainActivity.add_a_text_line((String) msg.obj);   // this prepends "\n"
-
-                break;
-		}
+		MainActivity.add_a_text_line((String) msg.obj);   // this prepends "\n
 	}
 }
